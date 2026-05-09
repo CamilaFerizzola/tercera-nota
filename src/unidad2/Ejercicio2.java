b91f3f7f4e5afa2ed82ec3aaa22fb757abf6a4cc
Camila Ferizzola <lferizzolasanchez@gmail.com>
2026-05-09 14:46:18 -0600

    tercera-nota

---
+++ src/unidad2/Ejercicio2.java
@@ -0,0 +1,38 @@
+package unidad2;
+import java.util.Scanner;
+public class Ejercicio2 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int numEmpleados;
+        double sueldo;
+        int empleados300 = 0;
+        int empleadosMas300 = 0;
+        double gastoTotal = 0;
+
+        System.out.print("Ingrese la cantidad de empleados: ");
+        numEmpleados = scanner.nextInt();
+        
+        gastoTotal += sueldo;
+
+        for (int i = 1; i <= numEmpleados; i++) {
+
+            System.out.print("Ingrese el sueldo del empleado " + i + ": ");
+            sueldo = scanner.nextDouble();
+
+            if (sueldo >= 100 && sueldo <= 300) {
+                empleados300++;
+            } else {
+                empleadosMas300++;
+            }
+        }
+        System.out.println("\nEmpleados que cobran entre 100 y 300 dólares: " + empleados300);
+        System.out.println("Empleados que cobran más de 300 dólares: " + empleadosMas300);
+        System.out.println("Gasto total en sueldos: $" + gastoTotal);
+
+        scanner.close();
+    }
+}
+
